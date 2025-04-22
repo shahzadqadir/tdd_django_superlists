402905817c6bd8a651c34b951c05bfce430104d4
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-04-22 09:46:36 +0100

    First unittest pass with url and a dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from django.urls import resolve
 
+from .views import homepage
 
 class SmokeTest(TestCase):
 
-    def test_smoke(self):
-        self.assertEqual(1+1, 2)
+    def test_root_url_resolves_to_home_page_view(self):
+        self.assertEqual(resolve('/').func, homepage)
