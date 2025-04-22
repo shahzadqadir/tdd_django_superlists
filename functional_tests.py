d314650f86d6c2ef88602ab411ca3b54ad281c45
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-04-22 09:16:40 +0100

    added unittesting

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,19 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Congratulations!' in browser.title
+class TestNewVisitor(unittest.TestCase):
 
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.response = self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
